e218a0968aeaebc2b8781284c5f9ebd03bf8ed2d
Hao Hoang <38025803+hdsme@users.noreply.github.com>
2025-07-11 19:50:45 -0700

    uupdate

--- PHEC-LSTM/train.py
+++ PHEC-LSTM/train.py
@@ -1,15 +1,13 @@
 import torch
 import torch.optim as optim
 from model import LSTMModel
-from dataset import build_dataset()
-
-
+from dataset import build_dataset
+from evaluation import build_eval
+ 
 MODEL_PATH = 'ltsm.pth'
 
 def train_ltsm():
-    model = LSTMModel(input_size=7, hidden_size=50, num_layers=1)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = model.to(device)
+
     criterion = nn.MSELoss()  # Thử nghiệm với nn.L1Loss() cho MAE
     optimizer = optim.Adam(model.parameters(), lr=0.001)  # Thử nghiệm với RMSprop, SGD
     # Early Stopping
@@ -23,7 +21,7 @@ def train_ltsm():
     for epoch in range(num_epochs):
         model.train()
         train_loss = 0
-        for X_batch, y_batch in train_loader:  # Giả sử train_loader đã được định nghĩa
+        for X_batch, y_batch in train:  # Giả sử train đã được định nghĩa
             X_batch, y_batch = X_batch.to(device), y_batch.to(device)
             optimizer.zero_grad()
             outputs = model(X_batch)
@@ -31,17 +29,17 @@ def train_ltsm():
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-        train_loss /= len(train_loader)
+        train_loss /= len(train)
 
         # Validation
         model.eval()
         val_loss = 0
         with torch.no_grad():
-            for X_batch, y_batch in val_loader:  # Giả sử val_loader đã được định nghĩa
+            for X_batch, y_batch in val:  # Giả sử val đã được định nghĩa
                 X_batch, y_batch = X_batch.to(device), y_batch.to(device)
                 outputs = model(X_batch)
                 val_loss += criterion(outputs.squeeze(), y_batch).item()
-            val_loss /= len(val_loader)
+            val_loss /= len(val)
 
         print(f'Epoch {epoch+1}, Train Loss: {train_loss:.6f}, Val Loss: {val_loss:.6f}')
 
@@ -55,6 +53,16 @@ def train_ltsm():
             if counter >= patience:
                 print("Early stopping")
                 break
+    return model
 
-# Tải mô hình tốt nhất
-model.load_state_dict(torch.load(MODEL_PATH))
+if __name__ == "__main__":
+    print("Bắt đầu huấn luyện LTSM...")
+    train_ltsm()
+
+    print("Kết thúc huấn luyện LTSM...")
+    print("Load mô hình LTSM...")
+    # Tải mô hình tốt nhất
+    model.load_state_dict(torch.load(MODEL_PATH))
+    print("Đánh giá mô LTSM...")
+    build_eval(model)
+
